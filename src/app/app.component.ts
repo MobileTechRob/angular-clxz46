61e3be476aa4e6c296405e277501fddbfeb372f8
MobileTechRob <rob.hermann@hotmail.com>
2019-08-10 20:10:59 -0500

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular';
+  name = 'Angular 123';
+  sportone = 'Bikes';
+  sporttwo = 'Shoes';
+  sportthree = 'Skis';
+
+  Bikes: any[] = [ {"Brand":"Specialized", "Price":"100"}, {"Brand":"Trek", "Price":"200"}, {"Brand":"Giant", "Price":"300"}, {"Brand":"E-Bike", "Price":"400"},{"Brand":"Izuno", "Price":"500"},{"Brand":"Cannondale", "Price":"600"}  ] 
 }
