d3576bc953640850d33202211ba9d47b68a4876c
MobileTechRob <rob.hermann@hotmail.com>
2019-08-10 20:52:14 -0500

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Sports ';
+  name:string = 'Sports ';
   sportone = 'Bikes';
   sporttwo = 'Shoes';
   sportthree = 'Skis';
