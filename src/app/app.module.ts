948188b7ab9890b58200601d5ca74e30989f59e3
MobileTechRob <rob.hermann@hotmail.com>
2019-09-08 18:23:38 -0500

    2 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello/hello.component';
 import { BikesComponent } from './bikes/bikes.component';
 import { ShoesComponent } from './shoes/shoes.component';
 import { SkiisComponent } from './skiis/skiis.component';
+import { ProductListComponentComponent } from './product-list-component/product-list-component.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, BikesComponent, ShoesComponent, SkiisComponent ],
+  imports:      [ BrowserModule, FormsModule, RouterModule.forRoot([
+      { path: '', component: ProductListComponent },
+      { path: 'products/:productId', component: ProductDetailsComponent },
+    ])],
+  declarations: [ AppComponent, HelloComponent, BikesComponent, ShoesComponent, SkiisComponent, ProductListComponentComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
