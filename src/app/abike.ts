cb4001be2ddd634b6724ef04c92df7925c442c45
MobileTechRob <rob.hermann@hotmail.com>
2019-10-22 21:11:04 -0500

    1 file modified, 1 added

---
+++ src/app/abike.ts
@@ -0,0 +1,6 @@
+export class ABike {
+  brand
+  model;
+  price;
+
+}
--- src/app/bike.service.ts
+++ src/app/bike.service.ts
@@ -2,10 +2,11 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class BikeService {
-  
+  Bikes: any[] = [ {"Brand":"Specialized", "Price":"100"}, {"Brand":"Trek", "Price":"200"}, {"Brand":"Giant", "Price":"300"}, {"Brand":"E-Bike", "Price":"400"},{"Brand":"Izuno", "Price":"500"},{"Brand":"Cannondale", "Price":"600"}  ] ;
 
   constructor() { }
 
   getBikes(){}
+  addBike(){}
 
 }
