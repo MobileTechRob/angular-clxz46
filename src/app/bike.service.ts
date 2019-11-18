b34d6775eec6dd4834ea0f3965bb08e78eaf8568
MobileTechRob <rob.hermann@hotmail.com>
2019-11-17 20:47:19 -0600

    2 files modified

--- src/app/bike.service.ts
+++ src/app/bike.service.ts
@@ -1,14 +1,20 @@
 import { Injectable } from '@angular/core';
-import { BikeModule } from './classobjects/bike/bike.module';
 
-@Injectable()
-export class BikeService {
-  Bikes: any[] = [ {"Brand":"Specialized", "Price":"100"}, {"Brand":"Trek", "Price":"200"}, {"Brand":"Giant", "Price":"300"}, {"Brand":"E-Bike", "Price":"400"},{"Brand":"Izuno", "Price":"500"},{"Brand":"Cannondale", "Price":"600"}  ] ;
+@Injectable({ providedIn: 'root'})
+export class BikeService 
+{
+  Bikes: any[] = [ {"Brand":"Specialized", "Price":"100"}, {"Brand":"Trek", "Price":"200"}, {"Brand":"Giant", "Price":"300"}, {"Brand":"E-Bike", "Price":"400"},{"Brand":"Izuno", "Price":"500"},{"Brand":"Cannondale", "Price":"600"} ] ;
 
-  constructor() {
-   }
+  constructor() 
+  {
 
-  getBikes(){}
+  }
+
+  getBikes()
+  {
+    return this.Bikes;
+  }
+
   addBike(){}
 
 }
--- src/app/bikes/bikes.component.ts
+++ src/app/bikes/bikes.component.ts
@@ -1,18 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { BikeService } from '../bike.service';
 
 @Component({
   selector: 'app-bikes',
   templateUrl: './bikes.component.html',
   styleUrls: ['./bikes.component.css']
 })
-export class BikesComponent implements OnInit {
 
-  Bikes: any[] = [ {"Brand":"Specialized", "Price":"100"}, {"Brand":"Trek", "Price":"200"}, {"Brand":"Giant", "Price":"300"}, {"Brand":"E-Bike", "Price":"400"},{"Brand":"Izuno", "Price":"500"},{"Brand":"Cannondale", "Price":"600"}  ] ;
+export class BikesComponent implements OnInit 
+{
+  //Bikes: any[] = [ {"Brand":"Specialized", "Price":"100"}, {"Brand":"Trek", "Price":"200"}, //{"Brand":"Giant", "Price":"300"}, {"Brand":"E-Bike", "Price":"400"},{"Brand":"Izuno", "Price":"500"},{"Brand":"Cannondale", "Price":"600"}  ] ;
 
-  constructor() { }
+  constructor()
+   {
+
+   }
 
   ngOnInit() 
   {
+  
   }
 
 }
