1eba89141db861c659d09ccaca92457a3b8abc4c
MobileTechRob <rob.hermann@hotmail.com>
2019-12-14 13:59:04 -0600

    2 files modified

--- src/app/bike.service.ts
+++ src/app/bike.service.ts
@@ -3,13 +3,16 @@ import { Injectable } from '@angular/core';
 @Injectable({ providedIn: 'root'})
 export class BikeService 
 {
-  Bikes: any[] = [ {"Brand":"Specialized", "Price":"100"}, {"Brand":"Trek", "Price":"200"}, {"Brand":"Giant", "Price":"300"}, {"Brand":"E-Bike", "Price":"400"},{"Brand":"Izuno", "Price":"500"},{"Brand":"Cannondale", "Price":"600"} ] ;
+  bikes: any[] = [ {"Brand":"Specialized", "Price":"100"}, {"Brand":"Trek", "Price":"200"}, {"Brand":"Giant", "Price":"300"}, {"Brand":"E-Bike", "Price":"400"},{"Brand":"Izuno", "Price":"500"},{"Brand":"Cannondale", "Price":"600"} ] ;
 
   getBikes()
   {
-    return this.Bikes;
+    return this.bikes;
   }
 
-  addBike(){}
+  addBike()
+  {
+
+  }
 
 }
