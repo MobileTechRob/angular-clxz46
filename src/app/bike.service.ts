ac29e53490de672700f74c8a3cc6cf95d51bbd52
MobileTechRob <rob.hermann@hotmail.com>
2019-12-14 15:59:22 -0600

    1 file modified, 1 added

--- src/app/bike.service.ts
+++ src/app/bike.service.ts
@@ -5,6 +5,8 @@ export class BikeService
 {
   bikes: any[] = [ {"Brand":"Specialized", "Price":"100"}, {"Brand":"Trek", "Price":"200"}, {"Brand":"Giant", "Price":"300"}, {"Brand":"E-Bike", "Price":"400"},{"Brand":"Izuno", "Price":"500"},{"Brand":"Cannondale", "Price":"600"} ] ;
 
+  newbikes: string[] = [];
+
   getBikes()
   {
     return this.bikes;
