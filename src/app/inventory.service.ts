b563f353a09147efb2730d8cc8db44df14cb6d5a
MobileTechRob <rob.hermann@hotmail.com>
2019-12-14 13:36:13 -0600

    1 file modified

--- src/app/inventory.service.ts
+++ src/app/inventory.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
 import { BikeService } from './bike.service';
-import { HiService } from './hi.service'
+import { HiService } from './hi.service';
 
 @Injectable()
 export class InventoryService 
 {
-
   constructor() { }
 
+  
 }
