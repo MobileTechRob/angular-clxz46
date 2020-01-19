d8be8f7988f21507f2b3026c8d3498e37cce0f2a
MobileTechRob <rob.hermann@hotmail.com>
2020-01-19 17:32:02 -0600

    2 files modified

--- src/app/shoe.service.ts
+++ src/app/shoe.service.ts
@@ -4,10 +4,14 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class ShoeService 
 {
-  Shoes: any[] = [ {"Brand":"Hoka", "Price":"100"}, {"Brand":"Saucony", "Price":"200"}, {"Brand":"Asics", "Price":"300"}, {"Brand":"Izuno", "Price":"400"},{"Brand":"Izuno", "Price":"500"}] ;
+  shoes: any[] = [ {"Brand":"Hoka", "Price":"100"}, {"Brand":"Saucony", "Price":"200"}, {"Brand":"Asics", "Price":"300"}, {"Brand":"Izuno", "Price":"400"},{"Brand":"Izuno", "Price":"500"}] ;
 
   constructor() { }
 
+  getshoes()
+  {
+    return this.shoes;
+  }
 
 
 
--- src/app/ski.service.ts
+++ src/app/ski.service.ts
@@ -3,8 +3,15 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class SkiService 
 {
-  Skiis: any[] = [ {"Brand":"Head", "Price":"100"}, {"Brand":"K2", "Price":"200"}, {"Brand":"Fischer", "Price":"300"}, {"Brand":"Salomon", "Price":"400"}] ;
+  skiis: any[] = [ {"Brand":"Head", "Price":"100"}, {"Brand":"K2", "Price":"200"}, {"Brand":"Fischer", "Price":"300"}, {"Brand":"Salomon", "Price":"400"}] ;
   
   constructor() { }
+
+  getSkiis()
+  {
+    return this.skiis;
+  }
+
+
 
 }
